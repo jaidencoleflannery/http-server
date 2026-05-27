2e776f204a7c076384f770ecf4bb5cd0e7b4ac3d
jaidencoleflannery <jaidencoleflannery@gmail.com>
2026-05-27 14:43:17 -0500

    sync | reverted git merge issues.

--- src/http-server/main.c
+++ src/http-server/main.c
@@ -5,14 +5,8 @@
 
 #include "services/logging/logging.h"
 #include "services/orchestrator/orchestrator.c"
-#include "utilities/host-configuration/host_configuration.c"
 
 int main(void) {
-    if(!configure_host()) {
-        ERROR_LOG("main: Failed to configure host.\n");
-        return EXIT_FAILURE;
-    }
-
     if(!boot_server()) {
         ERROR_LOG("main: Failed to boot.\n");
         return EXIT_FAILURE;
